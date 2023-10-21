a1998d0e3869bb278045c81357c7d956469c9150
RIZKIKOTET <rizkidhc31@gmail.com>
2023-10-21 15:10:03 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,3 +1,5 @@
 global.port = "5001";
 global.pairingNumber = ""; //pairing number to install bot
 global.sessionName = "session";
+
+//http://localhost:5001/send-message?message=tester&number=08123456789
